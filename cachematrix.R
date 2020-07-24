3595fabd3945a2434141dfa580158729e1f6f48e
tallulahspina <67917913+tallulahspina@users.noreply.github.com>
2020-07-24 15:56:44 -0300

    Update cachematrix.R
    makeCacheMatrix returns a list of functions:
            ## set the matrix (x)
            ## get the matrix (x)
            ## set the inverse (setsolve = m)
            ## get the inverse (getsolve = m)
    cachesolve  returns m if it has already been calculated, otherwise, calculate it

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <-function(x = matrix()){
+        ## return a list of functions:
+        ## set the matrix (x)
+        ## get the matrix (x)
+        ## set the inverse (setsolve = m)
+        ## get the inverse (getsolve = m)
+        ##      start everything saying we don´have m yet:
+        m <- NULL 
+        set <- function (x){
+                x <<- y
+                m <<- NULL
+                }
+        get <- function () x
+        setsolve <- function (solve) m <<- solve ## stablishes this goes to another environment
+        getsolve <- function () m
+        list(set=set, get=get, setsolve=setsolve, getsolve=getsolve)
+        ## this list will be used by cachesolve (not the matrix)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x = matrix(), ...) {
+        ## first, name things:
+        m <- x$getsolve ()
+        v <- matrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+                ## m if it has already been calculated
+        } ##otherwise, calculate it
+        new <-x$get()
+        v <- solve(new, ...)
+        v
 }
